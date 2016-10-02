5f306ad4aee3d53ebf3cbfa89af23e854d82b18f
Jared Burrows <jaredsburrows@gmail.com>
2016-10-02 14:48:30 -0700

    merge script and after_script

--- src/androidTest/java/burrows/apps/gif/example/TestApp.java
+++ src/androidTest/java/burrows/apps/gif/example/TestApp.java
@@ -7,11 +7,11 @@ import burrows.apps.gif.example.di.component.NetComponent;
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  */
 public class TestApp extends App {
-  public void setAppComponent(final AppComponent appComponent) {
+  public void setAppComponent(AppComponent appComponent) {
     this.appComponent = appComponent;
   }
 
-  public void setRiffsyComponent(final NetComponent riffsyComponent) {
+  public void setRiffsyComponent(NetComponent riffsyComponent) {
     this.netComponent = riffsyComponent;
   }
 }
