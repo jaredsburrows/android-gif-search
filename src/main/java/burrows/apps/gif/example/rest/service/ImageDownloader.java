ae2b44a81c522c765a4eb9fdc8a881af19aa83bb
Jared Burrows <jaredsburrows@gmail.com>
2016-10-16 13:08:30 -0700

    make images load faster

--- src/main/java/burrows/apps/gif/example/rest/service/ImageDownloader.java
+++ src/main/java/burrows/apps/gif/example/rest/service/ImageDownloader.java
@@ -7,14 +7,12 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import burrows.apps.gif.example.R;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.SimpleTarget;
-import pl.droidsonroids.gif.GifDrawable;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
-import java.io.IOException;
-import java.util.Arrays;
-
-import static android.content.ContentValues.TAG;
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
 
 /**
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
@@ -33,26 +31,35 @@ public class ImageDownloader {
     Glide.with(context)
       .load(url)
       .asGif()
-      .toBytes()
       .thumbnail(THUMBNAIL_MULTIPLIER)
       .override(GIF_IMAGE_WIDTH_PIXELS, GIF_IMAGE_HEIGHT_PIXELS)
-      .error(R.mipmap.ic_launcher)
-      .into(new SimpleTarget<byte[]>() {
-        @Override public void onResourceReady(byte[] resource, GlideAnimation<? super byte[]> glideAnimation) {
-          // Load gif
-          try {
-            imageView.setImageDrawable(new GifDrawable(resource));
-          } catch (IOException e) {
-            imageView.setImageResource(R.mipmap.ic_launcher);
-          }
+      .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+      .listener(new RequestListener<String, GifDrawable>() {
+        @Override public boolean onException(Exception e, String model, Target<GifDrawable> target, boolean isFirstResource) {
+          // Show gif
+          imageView.setImageResource(R.mipmap.ic_launcher);
           imageView.setVisibility(View.VISIBLE);
 
-          // Turn off progressbar
-          progressBar.setVisibility(View.INVISIBLE);
+          // Hide progressbar
+          progressBar.setVisibility(View.GONE);
           if (Log.isLoggable(TAG, Log.INFO)) {
-            Log.i(TAG, "finished loading\t" + Arrays.toString(resource));
+            Log.i(TAG, "finished loading\t" + model);
           }
+          return false;
         }
-      });
+
+        @Override public boolean onResourceReady(GifDrawable resource, String model, Target<GifDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+          // Show gif
+          imageView.setVisibility(View.VISIBLE);
+
+          // Hide progressbar
+          progressBar.setVisibility(View.GONE);
+          if (Log.isLoggable(TAG, Log.INFO)) {
+            Log.i(TAG, "finished loading\t" + model);
+          }
+          return false;
+        }
+      })
+      .into(imageView);
   }
 }
