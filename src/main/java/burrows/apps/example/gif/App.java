02b77a9d26a002f9ea11314c4f0ff38e8f4348b9
Jared Burrows <jaredsburrows@gmail.com>
2017-04-15 18:06:46 -0700

    fix lint issue

--- src/main/java/burrows/apps/example/gif/App.java
+++ src/main/java/burrows/apps/example/gif/App.java
@@ -1,5 +1,6 @@
 package burrows.apps.example.gif;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import burrows.apps.example.gif.presentation.di.component.ActivityComponent;
 import burrows.apps.example.gif.presentation.di.component.AppComponent;
@@ -7,6 +8,7 @@ import burrows.apps.example.gif.presentation.di.component.AppComponent;
 /**
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  */
+@SuppressLint("Registered")
 public class App extends Application {
   public AppComponent appComponent;
   public ActivityComponent activityComponent;
