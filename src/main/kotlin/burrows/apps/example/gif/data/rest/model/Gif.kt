1088bf37db5084e94b22e95d3d2d69272e1f5c69
Jared Burrows <jaredsburrows@gmail.com>
2017-06-04 13:54:36 -0700

    add kotlin with proguard

---
+++ src/main/kotlin/burrows/apps/example/gif/data/rest/model/Gif.kt
@@ -0,0 +1,59 @@
+package burrows.apps.example.gif.data.rest.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
+ */
+class Gif {
+    @SerializedName("url") private var url: String? = null
+    @SerializedName("preview") private var preview: String? = null
+
+    /**
+     * No args constructor for use in serialization
+     */
+    constructor() : this(Builder())
+
+    constructor(builder: Builder) {
+        this.url = builder.url
+        this.preview = builder.preview
+    }
+
+    fun url(): String? {
+        return url
+    }
+
+    fun preview(): String? {
+        return preview
+    }
+
+    fun newBuilder(): Builder {
+        return Builder(this)
+    }
+
+    class Builder {
+        var url: String? = null
+        var preview: String? = null
+
+        constructor()
+
+        constructor(gif: Gif) {
+            this.url = gif.url
+            this.preview = gif.preview
+        }
+
+        fun url(url: String): Builder {
+            this.url = url
+            return this
+        }
+
+        fun preview(preview: String): Builder {
+            this.preview = preview
+            return this
+        }
+
+        fun build(): Gif {
+            return Gif(this)
+        }
+    }
+}
