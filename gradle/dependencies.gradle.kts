eeebe62268e4c796b79687b7cf111bf646740226
Jared Burrows <jaredsburrows@gmail.com>
2018-03-13 18:42:57 -0700

    latest agp

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -1,5 +1,5 @@
 // common
-val androidGradleVersion            = "3.0.1"
+val androidGradleVersion            = "3.2.0-alpha06"
 val kotlinVersion                   = "1.2.30"
 val supportLibraryVersion           = "27.1.0"
 val daggerVersion                   = "2.15"
