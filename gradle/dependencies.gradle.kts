3ff5d83f66ab0ff6b1263da5c4f387c9d8bba6dd
Jared Burrows <jaredsburrows@gmail.com>
2018-08-10 06:45:36 -0700

    update truth to 0.42

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -77,6 +77,6 @@ extra["orchestrator"]               = "com.android.support.test:orchestrator:1.0
 extra["junit"]                      = "junit:junit:4.12"
 extra["mockitoInline"]              = "org.mockito:mockito-inline:2.21.0"
 extra["mockitoKotlin"]              = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
-extra["truth"]                      = "com.google.truth:truth:0.41"
+extra["truth"]                      = "com.google.truth:truth:0.42"
 extra["mockwebserver"]              = "com.squareup.okhttp3:mockwebserver:${okHttpVersion}"
 extra["reflections"]                = "org.reflections:reflections:0.9.11"
