ef33bf80629cfed4aa86c55555ef389943bb5e39
Jared Burrows <jaredsburrows@gmail.com>
2018-09-30 22:12:12 -0700

    update android gradle plugin to 3.3.0-alpha12

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -1,5 +1,5 @@
 // common
-val androidGradleVersion            = "3.3.0-alpha10"
+val androidGradleVersion            = "3.3.0-alpha12"
 val kotlinVersion                   = "1.2.71"
 val supportLibraryVersion           = "28.0.0"
 val daggerVersion                   = "2.17"
