b4660200aaab18f50328638c4c27debefadafb98
Jared Burrows <jaredsburrows@gmail.com>
2018-12-24 22:23:10 -0500

    update robolectric to 4.1

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -78,4 +78,4 @@ extra["mockitoKotlin"]              = "com.nhaarman.mockitokotlin2:mockito-kotli
 extra["truth"]                      = "com.google.truth:truth:0.42"
 extra["mockwebserver"]              = "com.squareup.okhttp3:mockwebserver:$okHttpVersion"
 extra["reflections"]                = "org.reflections:reflections:0.9.11"
-extra["robolectric"]                = "org.robolectric:robolectric:4.0.2"
+extra["robolectric"]                = "org.robolectric:robolectric:4.1"
