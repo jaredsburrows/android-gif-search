511030a122734fc73a4a91e5093457ce747faba4
Jared Burrows <jaredsburrows@gmail.com>
2019-06-08 10:50:24 -0700

    update robolectric to 4.3

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -77,4 +77,4 @@ extra["mockitoKotlin"]              = "com.nhaarman.mockitokotlin2:mockito-kotli
 extra["truth"]                      = "com.google.truth:truth:0.43"
 extra["mockwebserver"]              = "com.squareup.okhttp3:mockwebserver:$okHttpVersion"
 extra["reflections"]                = "org.reflections:reflections:0.9.11"
-extra["robolectric"]                = "org.robolectric:robolectric:4.2.1"
+extra["robolectric"]                = "org.robolectric:robolectric:4.3"
