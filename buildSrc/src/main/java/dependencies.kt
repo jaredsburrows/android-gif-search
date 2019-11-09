568bb4ab30d3f81035494e1c9faa107ff2ee4de4
Jared Burrows <jaredsburrows@gmail.com>
2019-11-09 13:01:43 -0800

    update okhttp to 4.2.2

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -7,7 +7,7 @@ object deps {
     const val androidGradle = "3.5.0-rc03"
     const val kotlin = "1.3.50"
     const val dagger = "2.24"
-    const val okHttp = "4.2.0"
+    const val okHttp = "4.2.2"
     const val moshi = "1.8.0"
     const val retrofit = "2.6.2"
     const val espresso = "3.2.0"
