cf1ed354d929f28c132380ca812b52fcaf21f0ed
Jared Burrows <jaredsburrows@gmail.com>
2019-11-20 23:22:56 -0800

    moshi 1.9.2

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -8,7 +8,7 @@ object deps {
     const val kotlin = "1.3.60"
     const val dagger = "2.25.2"
     const val okHttp = "4.2.2"
-    const val moshi = "1.8.0"
+    const val moshi = "1.9.2"
     const val retrofit = "2.6.2"
     const val espresso = "3.2.0"
     const val glide = "4.10.0"
