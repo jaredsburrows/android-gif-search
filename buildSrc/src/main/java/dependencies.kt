d78de38c959329a5f4130f9a38c9459699955af2
Jared Burrows <jaredsburrows@gmail.com>
2019-11-28 15:26:03 -0800

    Pr/jaredsburrows/updates 11 20 19 (#121)
    * gradle 6.0.1
    
    * ktlint gradle plugin 9.1.1
    
    * clean up gradle.properties
    
    * moshi 1.9.2

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -8,7 +8,7 @@ object deps {
     const val kotlin = "1.3.60"
     const val dagger = "2.25.2"
     const val okHttp = "4.2.2"
-    const val moshi = "1.8.0"
+    const val moshi = "1.9.2"
     const val retrofit = "2.6.2"
     const val espresso = "3.2.0"
     const val glide = "4.10.0"
@@ -34,7 +34,7 @@ object deps {
     const val dexcount = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:1.0.0"
     const val apksize = "com.vanniktech:gradle-android-apk-size-plugin:0.4.0"
     const val versions = "com.github.ben-manes:gradle-versions-plugin:0.27.0"
-    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.1.0"
+    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.1.1"
   }
 
   object kotlin {
