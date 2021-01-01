1f90cff6dd501c005bb3fc97dc87010815f108dc
Jared Burrows <jaredsburrows@gmail.com>
2020-12-31 23:53:44 -0500

    remove modified dependency updates task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -108,17 +107,4 @@ allprojects {
 
 apply {
   plugin("com.github.ben-manes.versions")
-}
-
-tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
-  fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-  }
-
-  rejectVersionIf {
-    isNonStable(candidate.version)
-  }
 }
