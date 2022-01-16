6ab94b69e7a7cc17e894a564b185b5c05551a038
Jared Burrows <jaredsburrows@gmail.com>
2022-01-16 12:54:32 -0500

    update few configurations about the build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+  repositories {
+    google()
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
