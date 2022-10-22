fcd9d496e317f99255274667c7389b3e668354da
Jared Burrows <jaredsburrows@gmail.com>
2022-10-22 11:46:20 -0400

    update glide deps

--- app/src/main/java/com/burrowsapps/example/gif/data/ImageService.kt
+++ app/src/main/java/com/burrowsapps/example/gif/data/ImageService.kt
@@ -1,12 +1,12 @@
 package com.burrowsapps.example.gif.data
 
 import android.content.Context
+import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.bumptech.glide.signature.ObjectKey
-import com.burrowsapps.example.gif.di.GlideApp
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,13 +19,15 @@ class ImageService @Inject internal constructor(@ApplicationContext private val 
     thumbnailUrl: String,
     override: Int = SIZE_ORIGINAL,
   ): RequestBuilder<GifDrawable> {
-    val request = GlideApp.with(context).asGif()
+    val request = Glide.with(context).asGif()
     return request
       .transition(withCrossFade())
       .thumbnail(
         request
           .transition(withCrossFade())
           .load(thumbnailUrl)
+          .override(override)
+          .signature(ObjectKey(thumbnailUrl))
       )
       .override(override)
       .signature(ObjectKey(imageUrl))
