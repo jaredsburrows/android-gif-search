1e3f820020f9e1104cd714aed8c5342be2b796b7
Jared Burrows <jaredsburrows@gmail.com>
2024-04-26 23:05:35 -0400

    Update CustomTestRunner to having logging

--- app/src/androidTest/java/com/burrowsapps/gif/search/test/CustomTestRunner.kt
+++ app/src/androidTest/java/com/burrowsapps/gif/search/test/CustomTestRunner.kt
@@ -2,8 +2,10 @@ package com.burrowsapps.gif.search.test
 
 import android.app.Application
 import android.content.Context
+import android.os.StrictMode
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
+import timber.log.Timber
 
 /**
  * Custom JUnit runner for Hilt tests.
@@ -19,6 +21,21 @@ class CustomTestRunner : AndroidJUnitRunner() {
     name: String?,
     context: Context?,
   ): Application {
+    Timber.plant(Timber.DebugTree())
+
+    StrictMode.setThreadPolicy(
+      StrictMode.ThreadPolicy.Builder()
+        .detectAll()
+        .penaltyLog()
+        .build(),
+    )
+    StrictMode.setVmPolicy(
+      StrictMode.VmPolicy.Builder()
+        .detectAll()
+        .penaltyLog()
+        .build(),
+    )
+
     return super.newApplication(cl, HiltTestApplication::class.java.name, context)
   }
 }
