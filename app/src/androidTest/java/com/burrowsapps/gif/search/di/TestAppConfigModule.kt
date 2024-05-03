55934e3f1def2a62e3fdccded4f00fb59e8099f6
Jared Burrows <jaredsburrows@gmail.com>
2024-05-03 01:36:32 -0400

    testinstallin AppConfigModule

--- app/src/androidTest/java/com/burrowsapps/gif/search/di/TestAppConfigModule.kt
+++ app/src/androidTest/java/com/burrowsapps/gif/search/di/TestAppConfigModule.kt
@@ -5,12 +5,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
 
 /** Injections for the API. */
 @Module
 @TestInstallIn(components = [SingletonComponent::class], replaces = [AppConfigModule::class])
 internal class TestAppConfigModule {
   @Provides
+  @Singleton
   fun provideApplicationMode(): ApplicationMode {
     return TESTING
   }
