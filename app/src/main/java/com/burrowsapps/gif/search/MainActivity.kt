d18a54907a6dc25995cdc35e60f3b1d2963f4e31
Jared Burrows <jaredsburrows@gmail.com>
2025-01-03 13:05:56 -0500

    Add edge to edge

--- app/src/main/java/com/burrowsapps/gif/search/MainActivity.kt
+++ app/src/main/java/com/burrowsapps/gif/search/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -30,6 +31,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
 
     setContent {
