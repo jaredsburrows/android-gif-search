3e72263ec5c4c9a564c8042235fa467d7d076a47
Jared Burrows <jaredsburrows@gmail.com>
2025-04-01 16:38:09 -0400

    fix theme

--- app/src/main/java/com/burrowsapps/gif/search/MainActivity.kt
+++ app/src/main/java/com/burrowsapps/gif/search/MainActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -82,9 +85,13 @@ fun MainScreen(
 ) {
   Scaffold(
     modifier = modifier,
+    contentWindowInsets = WindowInsets.systemBars,
   ) { innerPadding ->
     NavHost(
-      modifier = Modifier.padding(innerPadding),
+      modifier =
+        Modifier
+          .padding(innerPadding)
+          .consumeWindowInsets(WindowInsets.systemBars),
       navController = navController,
       startDestination = Screen.Gif.route,
     ) {
--- app/src/main/java/com/burrowsapps/gif/search/ui/theme/Theme.kt
+++ app/src/main/java/com/burrowsapps/gif/search/ui/theme/Theme.kt
@@ -1,46 +1,20 @@
 package com.burrowsapps.gif.search.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat.getInsetsController
 
 @Composable
 fun GifTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  dynamicColor: Boolean = true,
   content: @Composable () -> Unit,
 ) {
+  val context = LocalContext.current
   val colorScheme =
-    when {
-      dynamicColor -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> darkColorScheme() // default
-      else -> lightColorScheme() // default
-    }
-  val view = LocalView.current
-  if (!view.isInEditMode) {
-    SideEffect {
-      (view.context as Activity).apply {
-        this.window.statusBarColor = colorScheme.background.toArgb()
-        getInsetsController(this.window, view).apply {
-          isAppearanceLightStatusBars = !darkTheme
-          isAppearanceLightNavigationBars = !darkTheme
-        }
-      }
-    }
-  }
+    if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
 
   MaterialTheme(
     colorScheme = colorScheme,
