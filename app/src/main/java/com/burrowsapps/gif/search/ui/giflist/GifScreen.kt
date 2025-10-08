1757edd424e1c09823c02f6ecb73e0e3de06a54d
Jared Burrows <jaredsburrows@gmail.com>
2025-10-08 11:43:57 -0400

    hiltViewModel was moved (#1081)

--- app/src/main/java/com/burrowsapps/gif/search/ui/giflist/GifScreen.kt
+++ app/src/main/java/com/burrowsapps/gif/search/ui/giflist/GifScreen.kt
@@ -73,7 +73,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.LoadState
