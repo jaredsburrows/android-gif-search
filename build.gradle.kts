9fe7e413c90708cebd4368a1be47b9351ebf8e4d
Jared Burrows <jaredsburrows@gmail.com>
2026-02-01 18:45:49 -0500

    migrate to 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ import java.util.Locale
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
-  alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.ksp) apply false
   alias(libs.plugins.dagger) apply false
