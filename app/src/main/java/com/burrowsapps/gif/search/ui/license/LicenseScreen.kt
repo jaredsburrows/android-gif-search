95c2c6ef0a2668e250b28d6e165fb033fa6b706d
Jared Burrows <jaredsburrows@gmail.com>
2026-02-11 21:50:39 -0500

    clean up lint (#1215)

--- app/src/main/java/com/burrowsapps/gif/search/ui/license/LicenseScreen.kt
+++ app/src/main/java/com/burrowsapps/gif/search/ui/license/LicenseScreen.kt
@@ -13,11 +13,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -114,11 +119,17 @@ private fun TheToolbar(
     },
     navigationIcon = {
       if (navController.previousBackStackEntry != null) {
-        IconButton(onClick = { navController.navigateUp() }) {
-          Icon(
-            imageVector = ArrowBack24,
-            contentDescription = stringResource(id = R.string.menu_back_content_description),
-          )
+        TooltipBox(
+          positionProvider = TooltipDefaults.rememberTooltipPositionProvider(TooltipAnchorPosition.Above),
+          tooltip = { PlainTooltip { Text(stringResource(R.string.menu_back_content_description)) } },
+          state = rememberTooltipState(),
+        ) {
+          IconButton(onClick = { navController.navigateUp() }) {
+            Icon(
+              imageVector = ArrowBack24,
+              contentDescription = stringResource(id = R.string.menu_back_content_description),
+            )
+          }
         }
       }
     },
